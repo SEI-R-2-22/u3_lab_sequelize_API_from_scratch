cbe12550d79b420ad62e4ba75a7b002d0665cd1c
Mark Ryan <markrryan99@gmail.com>
2022-04-12 13:12:19 -0700

    set up complete

---
+++ migrations/20220412194956-create-pets.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('pets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      petName: {
+        type: Sequelize.STRING
+      },
+      petBreed: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('pets');
+  }
+};
