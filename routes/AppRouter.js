7e2c5fa0a18a075f1d0cb8f55199972c67c82848
David Wu <dwu0508@gmail.com>
2022-04-12 16:13:53 -0500

    added controllers for property

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,9 +1,9 @@
 const Router = require('express').Router()
 const PropertyRouter = require('./PropertyRouter')
-const AddressRouter = require('./AddressRouter')
-const TenantRouter = require('./TenantRouter')
+// const AddressRouter = require('./AddressRouter')
+// const TenantRouter = require('./TenantRouter')
 Router.use('/property', PropertyRouter)
-Router.use('/address', AddressRouter)
-Router.use('/tenant', TenantRouter)
+// Router.use('/address', AddressRouter)
+// Router.use('/tenant', TenantRouter)
 
 module.exports = Router
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const app = require('express')()
-const cors = require('cors')
 const express = require('express')
+const app = express()
+const cors = require('cors')
 
-const AppRouter = required('./routes/AppRouter')
+const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
