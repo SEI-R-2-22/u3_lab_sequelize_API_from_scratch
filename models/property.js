59f0a7f828943689f3121c7ddb567230d73b2a15
David Wu <dwu0508@gmail.com>
2022-04-12 13:51:00 -0500

    finished models

---
+++ models/property.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Property extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Property.hasOne(models.Address, {
+        foreignKey: 'propertyId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
+      Property.hasMany(models.Tenant, {
+        foreignKey: 'propertyId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
+    }
+  }
+  Property.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Property',
+    tableName: 'properties'
+  });
+  return Property;
+};
