23b21f29b3c0b07920fd4987f98dfc6bce4bdb83
ryan.ditzel <ryand370@gmail.com>
2022-04-12 14:56:40 -0400

    fixed migrations and migrated all files correctly

--- migrations/20220412182230-create-property.js
+++ migrations/20220412182230-create-property.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Properties', {
+    await queryInterface.createTable('properties', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Properties');
+    await queryInterface.dropTable('properties');
   }
 };
--- migrations/20220412182721-create-tenant.js
+++ migrations/20220412182721-create-tenant.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tenants', {
+    await queryInterface.createTable('tenants', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Tenants');
+    await queryInterface.dropTable('tenants');
   }
 };
