9e390fae00d090c79d4ef4548841a630d5a437e9
David Wu <dwu0508@gmail.com>
2022-04-12 15:05:48 -0500

    before seed

--- migrations/20220412182454-create-address.js
+++ migrations/20220412182454-create-address.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Addresses', {
+    await queryInterface.createTable('addresses', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Addresses');
+    await queryInterface.dropTable('addresses');
   }
 };
---
+++ routes/PropertyRouter.js
@@ -0,0 +1,7 @@
+const Router =require('express').Router()
+const controller = require('../controllers/PropertyController')
+
+Router.get('/', controller.GetAllProperties)
+Router.get('/:property_id',controller.GetProperty)
+
+module.exports = Router
