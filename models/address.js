dd64e6d4073f202ee977978002b0992bc752ea5c
ryan.ditzel <ryand370@gmail.com>
2022-04-12 14:51:06 -0400

    set up models

---
+++ models/address.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Address.belongsTo(models.Property, {
+        foreignKey: 'propertyId'
+      })
+    }
+  }
+  Address.init({
+    street: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    propertyId: {
+      type: DataTypes.INTEGER,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'properties',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Address',
+    tableName: 'addresses'
+  });
+  return Address;
+};
