bcdf9f0a16a9fdc9837c5e9d8843358536c8dab3
Yusong Shi <jackshi.sys@gmail.com>
2022-04-12 22:00:36 -0400

    update

--- seeders/20220412193416-user.js
+++ seeders/20220412193416-user.js
@@ -1,20 +1,20 @@
-// 'use strict'
-// const falso = require('@ngneat/falso')
+'use strict'
+const falso = require('@ngneat/falso')
 
-// const users = [...Array(100)].map(() => ({
-//   lastName: falso.randLastName(),
-//   firstName: falso.randFirstName(),
-//   email: falso.randEmail(),
-//   password: falso.randPassword(),
-//   createdAt: new Date(),
-//   updatedAt: new Date()
-// }))
-// module.exports = {
-//   up: async (queryInterface, Sequelize) => {
-//     return queryInterface.bulkInsert('users', users)
-//   },
+const users = [...Array(100)].map(() => ({
+  lastName: falso.randLastName(),
+  firstName: falso.randFirstName(),
+  email: falso.randEmail(),
+  password: falso.randPassword(),
+  createdAt: new Date(),
+  updatedAt: new Date()
+}))
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', users)
+  },
 
-//   down: async (queryInterface, Sequelize) => {
-//     return queryInterface.bulkDelete('users')
-//   }
-// }
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users')
+  }
+}
