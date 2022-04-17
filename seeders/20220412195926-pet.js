4ed9532b37b541f574cb18e43525a08c67fbe083
Mark Ryan <markrryan99@gmail.com>
2022-04-17 09:43:56 -0700

    forgotto actually seed my pets lol

--- seeders/20220412195926-pet.js
+++ seeders/20220412195926-pet.js
@@ -1,24 +1,24 @@
 'use strict';
+const { User, sequelize, Sequelize } = require('../models')
+const falso = require('@ngneat/falso');
+const { Client } = require('pg/lib');
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    const pets = await Promise.all(
+      [...Array(12)].map(async () => {
+        let client = await Client.findOne({ order: sequelize.random(), raw:true })
+        return {
+          content: falso.randDog(),
+          client_id: client.id
+        }
+      })
+    )
+    return queryInterface.bulkInsert('pets', pets)
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('pets')
+  
   }
 };
